6f76132f134b74ba87a0838d78b02a3850e216fa
mamad67M <67258471+mamad67M@users.noreply.github.com>
2026-09-10 18:08:30 +0200

    Refactor Firestore initialization in firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,21 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-
 const firebaseConfig = {
-
   apiKey: "AIzaSyA5VEBt6kE8vznP3M1fnSuIqlmj1jShdsw",
   authDomain: "mesurepro.firebaseapp.com",
   projectId: "mesurepro",
   storageBucket: "mesurepro.firebasestorage.app",
   messagingSenderId: "247001259728",
   appId: "1:247001259728:web:81b07368e0daf8d8e448d5"
-
 };
-
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app, "ai-studio-mesurepro-632ac469-c5d1-476c-aa0e-9d0e771c1927");
-
+export const db = getFirestore(app);
